c74905b8db5829cc6acb5e141a4d9670d2ae50c7
Victor Robinson <victorandtina@hotmail.com>
2015-06-18 16:24:33 -0400

    Changing so that extensions default to living in .build folder. Scanning the packages folder took too long and could result in path too long errors.

--- Source/tools/init.ps1
+++ Source/tools/init.ps1
@@ -1,7 +1,7 @@
 param($installPath, $toolsPath, $package, $project)
 
-# There is no equivilent uninstall because NuGet does not support it: http://nuget.codeplex.com/workitem/2074
+# There is no equivalent uninstall because NuGet does not support it: http://nuget.codeplex.com/workitem/2074
 
 $destinationPath = (get-item $installPath).parent.parent.FullName
-copy-item "$installPath\Build.proj" "$destinationPath\Build.proj" -recurse -force
-copy-item "$installPath\Projects.targets" "$destinationPath\Projects.targets" -recurse -force
+copy-item "$installPath\Build.proj" "$destinationPath\Build.proj" -force
+copy-item "$installPath\Projects.targets" "$destinationPath\Projects.targets" -force
